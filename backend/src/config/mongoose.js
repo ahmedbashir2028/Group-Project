1cd18ed103cb7bfc021f2c4272bf86b79995b5e7
arsalansultan <arsalarslaan7@gmail.com>
2022-12-30 10:41:30 +0500

    title  change

--- backend/src/config/mongoose.js
+++ backend/src/config/mongoose.js
@@ -1,19 +1,26 @@
-const mongoose = require('mongoose');
-const { mongo } = require('./vars');
-
+const mongoose = require("mongoose");
+const { mongo } = require("./vars");
+const { MONGO_URLs } = require("./vars");
 /**
-* Connect to mongo db
-*
-* @returns {object} Mongoose connection
-* @public
-*/
+ * Connect to mongo db
+ *
+ * @returns {object} Mongoose connection
+ * @public
+ */
 exports.connect = () => {
-  mongoose.connect(mongo.uri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-    keepAlive: 1,
-  });
+  mongoose
+    .connect(
+      "mongodb://ahmad:9HuwXD6V6fDgbCMa@ac-zrx1llj-shard-00-00.hs0ys0f.mongodb.net:27017,ac-zrx1llj-shard-00-01.hs0ys0f.mongodb.net:27017,ac-zrx1llj-shard-00-02.hs0ys0f.mongodb.net:27017/?ssl=true&replicaSet=atlas-be17az-shard-0&authSource=admin&retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        keepAlive: 1,
+      }
+    )
+    .catch((err) => {
+      console.log(err);
+    });
   return mongoose.connection;
 };
--- backend/src/config/vars.js
+++ backend/src/config/vars.js
@@ -3,8 +3,12 @@ require("dotenv").config();
 
 module.exports = {
   port: 5000,
+  MONGO_URL: "",
+
   mongo: {
-    uri: "mongodb://localhost:27017/FP",
+    // uri: "mongodb://localhost:27017/FP",
+    // MONGO_URL:
+    // "mongodb+srv://admin:WtlH6EXyUl5mYwDu@cluster0.hs0ys0f.mongodb.net/Shopit?retryWrites=true&w=majority",
   },
 };
 // module.exports = {
